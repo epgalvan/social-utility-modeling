1205466b103b0a82280e6bb7f39f0004ed07e86e
epgalvan <128032945+epgalvan@users.noreply.github.com>
2023-06-17 12:39:49 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Social Utility Modeling'
+copyright = '2023, Elijah Galvan'
+author = 'Elijah Galvan'
 
 release = '0.1'
 version = '0.1.0'
