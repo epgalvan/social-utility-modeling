6e237fb7b4ebabeee8adbb29abc68b60386cb1f9
Elijah Galvan <epgalvan20@g.ucla.edu>
2023-06-19 14:32:03 +0200

    reverting to width

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,16 +31,5 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static']
-
-# override wide tables in RTD theme
-html_context = {
-    'css_files': [        
-        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-        '_static/theme_overrides.css',
-    ],
-}
-
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
