50d7e54489a9e51838afc5c85434dfb2440f6a00
Elijah Galvan <epgalvan20@g.ucla.edu>
2023-06-19 14:01:58 +0200

    retry conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,9 @@ html_static_path = ['_static']
 
 # override wide tables in RTD theme
 html_context = {
-    'css_files': [
+    'css_files': [        
+        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
         '_static/theme_overrides.css',
     ],
 }
